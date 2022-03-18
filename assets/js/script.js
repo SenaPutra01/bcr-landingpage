894dcad00762abc16a66e12289d244671b8f2d2a
Sena Putra <senaaputra@gmail.com>
2022-03-18 14:56:56 +0700

    [Sena] edit navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,45 @@
+(function mainScript() {
+  "use strict";
 
-/* SCROLL NAVBAR */ 
+  const offcanvasToggle = document.querySelector(
+    '[data-bs-toggle="offcanvas"]'
+  );
+  const offcanvasCollapse = document.querySelector(".offcanvas-collapse");
+
+  offcanvasToggle.addEventListener("click", function () {
+    offcanvasCollapse.classList.toggle("open");
+  });
+})();
+/* SCROLL NAVBAR */
 $(() => {
-    $('.new-card').slice(0, 4).show()
-    $('.load-more').click(function (e) {
-        e.preventDefault()
-        $('.new-card:hidden').slice(0, 4).slideDown()
-    })
-})
+  $(".new-card").slice(0, 4).show();
+  $(".load-more").click(function (e) {
+    e.preventDefault();
+    $(".new-card:hidden").slice(0, 4).slideDown();
+  });
+});
 
-/* ACTIVE LINK NAVBAR */ 
+/* ACTIVE LINK NAVBAR */
 $(document).ready(function () {
-    $('.nav-link').click(function () {
-        $('.nav-link').removeClass('active')
-        $(this).addClass('active')
-    })
-})
+  $(".nav-link").click(function () {
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+  });
+});
 
-
-/* SLIDER */ 
+/* SLIDER */
 var swiper = new Swiper(".mySwiperPopuler", {
-    grabCursor: true,
-    spaceBetween: 20,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    autoplay: {
-        delay: 5000,
-    },
-    navigation: {
-        nextEl: ".bxs-chevron-right-circle",
-        prevEl: ".bxs-chevron-left-circle",
-    },
-
+  grabCursor: true,
+  spaceBetween: 20,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: ".bxs-chevron-right-circle",
+    prevEl: ".bxs-chevron-left-circle",
+  },
 });
 
 // AOS
